b35a29334e6f95370da8625c98c5086317ec04b6
Daholli <christoph.hollizeck@web.de>
2021-08-10 16:58:28 +0200

    added cat_near_mouse

--- scripts/cat.py
+++ scripts/cat.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class Cat:
     def __init__(self, cheese_array):
         self.cheese_array = cheese_array
@@ -22,3 +25,20 @@ class Cat:
     def plan_movement(self):
         "plan next moves of cat"
         pass
+
+    def cat_near_mouse(self, mouse_position, mouse_speed, threshold):
+        """Is the cat close the the mouse and warrants a change of plans
+
+        Args:
+            mouse_position (np.array): mouse position
+            mouse_speed (float): maxspeed of the mouse
+            threshold (float): time threshold to consider cat close
+
+        Returns:
+            bool: bool representing "closeness"
+        """
+        distance = np.linalg.norm(mouse_position - self.position)
+        time = (distance / self.speed) * (self.speed / mouse_speed)
+
+        # alternatively could return the time and work with this (if time to reach cheese < time for cat to catch mouse don't change plans)
+        return True if time < threshold else False
