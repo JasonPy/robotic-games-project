505d78ad5e2c8ae10685c52498ed6103b111af93
Daholli <christoph.hollizeck@web.de>
2021-08-10 15:23:39 +0200

    inital model of cat

---
+++ scripts/cat.py
@@ -0,0 +1,25 @@
+class Cat:
+    def __init__(self, cheese_array, cat_position):
+        self.position = cat_position
+        self.cheese_array = cheese_array
+        self.max_speed = 0
+        self.max_angular = 0
+
+    def update(self, cat_position, mouse_position, speed, angular):
+        """Update all variables of interest.
+
+        Args:
+            cat_position (np.array): updated position of cat
+            mouse_position (np.array): updated position of mouse
+            speed (float): current speed of cat
+            angular (float): current angular velocity of cat
+        """
+        self.position = cat_position
+        self.mouse_position = mouse_position
+
+        self.max_speed = speed if speed > self.max_speed else self.max_speed
+        self.max_angular = angular if angular > self.max_angular else self.max_angular
+
+    def plan_movement(self):
+        "plan next moves of cat"
+        pass
