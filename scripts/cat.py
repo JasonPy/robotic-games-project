19210a85d9ce048b24120d117508e57be47db70f
MLitzinger <marie.litzinger@gmail.com>
2021-08-14 12:59:51 +0200

    Refactored some code and started to include estimation for cat speed and angular velocity

--- scripts/cat.py
+++ scripts/cat.py
@@ -4,23 +4,32 @@ import numpy as np
 class Cat:
     def __init__(self, cheese_array):
         self.cheese_array = cheese_array
-        self.max_speed = 0
-        self.max_angular = 0
+        self.position = np.zeros(2)
+        self.max_speed = 0.3  # smallest possible value = 0.2, highest possible value 0.4
+        self.max_angular = 1  # smallest possible value = -2, highest possible value 2
 
-    def update(self, cat_position, mouse_position, speed, angular):
+    def update(self, cat_position):
         """Update all variables of interest.
 
         Args:
             cat_position (np.array): updated position of cat
-            mouse_position (np.array): updated position of mouse
-            speed (float): current speed of cat
-            angular (float): current angular velocity of cat
         """
+        velocities = self.calculate_speed(cat_position)
+        speed = velocities[0]
+        angular = velocities[1]
+
         self.position = cat_position
-        self.mouse_position = mouse_position
 
         self.max_speed = speed if speed > self.max_speed else self.max_speed
         self.max_angular = angular if angular > self.max_angular else self.max_angular
+
+    def calculate_speed(self, new_position):
+        """
+        This function estimates the angular and linear velocity of the cat given the prior and the current position.
+        :param new_position: current position of the cat
+        """
+        velocities = np.zeros(2)
+        return velocities
 
     def plan_movement(self):
         "plan next moves of cat"
@@ -38,7 +47,7 @@ class Cat:
             bool: bool representing "closeness"
         """
         distance = np.linalg.norm(mouse_position - self.position)
-        time = (distance / self.speed) * (self.speed / mouse_speed)
+        time = (distance / self.max_speed) * (self.max_speed / mouse_speed)
 
         # alternatively could return the time and work with this (if time to reach cheese < time for cat to catch mouse don't change plans)
         return True if time < threshold else False
